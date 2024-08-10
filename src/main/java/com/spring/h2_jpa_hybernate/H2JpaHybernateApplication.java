43ef3aa1bbd019b14a114e5fa13e1fb31d6fa7d5
subramanyam123-c <subramanyamb30@gmail.com>
2024-08-09 20:15:28 -0500

    first commit

---
+++ src/main/java/com/spring/h2_jpa_hybernate/H2JpaHybernateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.h2_jpa_hybernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2JpaHybernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2JpaHybernateApplication.class, args);
+	}
+
+}
