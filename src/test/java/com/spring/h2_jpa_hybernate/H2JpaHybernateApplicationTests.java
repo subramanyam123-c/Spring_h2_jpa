ca64e4791db4db8297ca3e90f2c49fa5496e1f38
subramanyam123-c <subramanyamb30@gmail.com>
2024-08-09 20:15:28 -0500

    first commit

---
+++ src/test/java/com/spring/h2_jpa_hybernate/H2JpaHybernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.h2_jpa_hybernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2JpaHybernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
